ba9b017adf0217f5a5ee7991438e310501421212
elahe <117848923+elahetaqdimi125@users.noreply.github.com>
2024-01-01 12:09:22 +0330

    change file

--- src/component/slider.js
+++ src/component/slider.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import './slider.css'
 
-// var slideIndex=0;
-// // Slider();
 function Slider() {
-// var i;
-// var slides=document.getElementsByClassName('item card');
-// var dot=document.getElementsByClassName('dot');
-// for(i=0;i<slides.length;i++){
-//   slides[i].style.display='none';
-// }
-// slideIndex++;
-// if(slideIndex>slides.length){
-//   slideIndex=1;
-// }
-// for(i=0;i<dot.length;i++){
-//   dot[i].className=dot[i].className.replace('active','')
-// }
-// slides[slideIndex-1].style.display='block';
-// dot[slideIndex-1].className+=active;
-// setTimeout(Slider,2000);
-// }
+
  return (
 <>
 <section className="stats">
@@ -29,7 +11,9 @@ function Slider() {
               <h3>Our hardwares</h3>
             </div> 
           </div>
+    
           <div className="carousel ">
+    
             <input type="radio" name="position" checked const className='dot'/>
             <input type="radio" name="position" className='dot'/>
             <input type="radio" name="position" className='dot'/>
@@ -57,7 +41,7 @@ function Slider() {
                 <p>Begin your journey to IELTS success by signing up for a WriteWiseAI account. Once you've registered, log in to access your user panel and explore our wide range of features tailored for IELTS test-takers</p>
               </div>
               </main>
-         
+            
      </div>
     </section>
 </>
