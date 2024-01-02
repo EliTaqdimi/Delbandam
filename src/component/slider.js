38e295b9dec1ab03e7470cccabf1189d43d77857
elahe <117848923+elahetaqdimi125@users.noreply.github.com>
2024-01-02 10:22:44 +0330

    create slider new

--- src/component/slider.js
+++ src/component/slider.js
@@ -1,49 +1,26 @@
 import React from 'react';
-import './slider.css'
+import './slider.css';
 
-function Slider() {
+
+function Slider(props) {
 
  return (
 <>
-<section className="stats">
-          <div className="container">
-            <div className="stats-heading text-center my-1">
-              <h3>Our hardwares</h3>
-            </div> 
-          </div>
+<div className="slider-container">
+  <div className="left-slide">
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+    </div>
     
-          <div className="carousel ">
+  </div>
+  <div className="right-slide">
+    <div className="div-hard "style={{textAlign: 'center', marginTop:'5rem'}} >
+      <img src={props.url} className="img-hard" alt=''/>
+      </div>
     
-            <input type="radio" name="position" checked const className='dot'/>
-            <input type="radio" name="position" className='dot'/>
-            <input type="radio" name="position" className='dot'/>
-            <input type="radio" name="position" className='dot'/>
-      
-            <main id="carousel">
-              <div className="item card">
-               <img src={require('../images/hard-1.48aa129.png')} alt='' style={{width:'100px',height:'100px'}}/>
-                <h2>Sign Up and Log InSign Up and Log In</h2>
-                <p>Begin your journey to IELTS success by signing up for a WriteWiseAI account. Once you've registered, log in to access your user panel and explore our wide range of features tailored for IELTS test-takers</p>
-              </div>
-              <div class="item card">
-              <img src={require('../images/hard-2.bba8b7f.png')} alt=''/>
-                <h2>Sign Up and Log InSign Up and Log In</h2>
-                <p>Begin your journey to IELTS success by signing up for a WriteWiseAI account. Once you've registered, log in to access your user panel and explore our wide range of features tailored for IELTS test-takers</p>
-              </div>
-              <div className="item card">
-              <img src={require('../images/hard-3.6c06644.png')} alt=''/>
-                <h2>Sign Up and Log InSign Up and Log In</h2>
-                <p>Begin your journey to IELTS success by signing up for a WriteWiseAI account. Once you've registered, log in to access your user panel and explore our wide range of features tailored for IELTS test-takers</p>
-              </div>
-              <div className="item card">
-              <img src={require('../images/hard-4.b7115f1.png')} alt=''/>
-                <h2>Sign Up and Log InSign Up and Log In</h2>
-                <p>Begin your journey to IELTS success by signing up for a WriteWiseAI account. Once you've registered, log in to access your user panel and explore our wide range of features tailored for IELTS test-takers</p>
-              </div>
-              </main>
-            
-     </div>
-    </section>
+  </div>
+</div>
 </>
   );
 }
