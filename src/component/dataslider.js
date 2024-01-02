578ce38d980da1f5d8436e3742408eca055f264c
elahe <117848923+elahetaqdimi125@users.noreply.github.com>
2024-01-02 10:22:11 +0330

    change data slider Our hardwares

---
+++ src/component/dataslider.js
@@ -0,0 +1,50 @@
+import hard from '../images/hard-1.48aa129.png';
+import hard2 from '../images/hard-2.bba8b7f.png';
+import hard3 from '../images/hard-3.6c06644.png';
+import hard4 from '../images/hard-4.b7115f1.png'
+export const respons = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 1024 },
+    items: 1,
+    slidesToSlide: 1
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 800 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max:800, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+export const slidetData = [
+  {
+    id: 1,
+    imageurl:hard,
+    name: "Up and Log ",
+    description: 'Begin your journey to IELTS success by signing up for a WriteWiseAI account. Once youve registered, log in to access your user panel and explore our wide range of features tailored for IELTS test-takers'
+  },
+  {
+    id: 2,
+    imageurl:hard2,
+    name: "Up and Log ",
+    description: 'Begin your journey to IELTS success by signing up for a WriteWiseAI account. Once youve registered, log in to access your user panel and explore our wide range of features tailored for IELTS test-takers'
+  },
+  {
+    id: 3,
+    imageurl:hard3,
+    name: "Up and Log ",
+    description: 'Begin your journey to IELTS success by signing up for a WriteWiseAI account. Once youve registered, log in to access your user panel and explore our wide range of features tailored for IELTS test-takers'
+  },
+  {
+    id: 4,
+    imageurl:hard4,
+    name: "Up and Log ",
+    description: 'Begin your journey to IELTS success by signing up for a WriteWiseAI account. Once youve registered, log in to access your user panel and explore our wide range of features tailored for IELTS test-takers'
+  },
+]
